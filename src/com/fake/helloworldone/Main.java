004195b93ca074dd6ef9f9e12b3a71fea216f3ec
Rafael <rafael.rabelo@venturus.org.br>
2013-05-20 18:59:10 -0300

    initial commit

---
+++ src/com/fake/helloworldone/Main.java
@@ -0,0 +1,12 @@
+package com.fake.helloworldone;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("One");
+    }
+
+}
