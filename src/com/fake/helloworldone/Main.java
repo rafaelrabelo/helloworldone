a113ce65c0e5216504f5bc59e205110860199e18
Rafael <rafael.rabelo@venturus.org.br>
2013-05-20 19:21:04 -0300

    print message modified

--- src/com/fake/helloworldone/Main.java
+++ src/com/fake/helloworldone/Main.java
@@ -6,7 +6,7 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("One");
+        System.out.println("Hello World One");
     }
 
 }
