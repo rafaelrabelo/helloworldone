f0838aaaf524640592a229c4dd535365a077e3b2
Rafael <rafael.rabelo@venturus.org.br>
2013-05-21 08:20:31 -0300

    egit first commit

--- src/com/fake/helloworldone/Main.java
+++ src/com/fake/helloworldone/Main.java
@@ -8,7 +8,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World One");
         System.out.println("modified from original project");
+        System.out.println("first commit with egit");
         System.out.println("y");
+        
     }
 
 }
